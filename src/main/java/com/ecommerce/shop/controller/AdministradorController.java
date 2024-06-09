93c039e2f3981b3b61341daaa3bf64c390ef7846
ahpineda <alejandrohectorpineda@gmail.com>
2024-06-09 00:41:30 -0300

    home admin con productos

--- src/main/java/com/ecommerce/shop/controller/AdministradorController.java
+++ src/main/java/com/ecommerce/shop/controller/AdministradorController.java
@@ -1,10 +1,15 @@
-/*
+    /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package com.ecommerce.shop.controller;
 
+import com.ecommerce.shop.model.Producto;
+import com.ecommerce.shop.service.ProductoService;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -15,8 +20,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/administrador")
 public class AdministradorController {
+    @Autowired
+    private ProductoService productoservice;
     @GetMapping("")
-    public String home(){
+    public String home(Model model){
+        List<Producto> productos = productoservice.findAll();
+        model.addAttribute("productos",productos);
         return "administrador/home";
     }
 }
